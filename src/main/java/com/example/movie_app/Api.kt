fe1611323fcb3982e8999e224cdc1787965a17ae
Angela <56811978+SUJINJEONG012@users.noreply.github.com>
2022-10-06 14:05:05 +0900

    Update Api.kt

--- src/main/java/com/example/movie_app/Api.kt
+++ src/main/java/com/example/movie_app/Api.kt
@@ -9,7 +9,7 @@ interface Api {
     //인기있는
     @GET("movie/popular")
     fun getPopularMovies(
-        @Query("api_key") apiKey: String = "b0b56c166ec5839d540382c3efb9113e",
+        @Query("api_key") apiKey: String = "키발급",
         @Query("page") page: Int,
         @Query("language") language : String = "ko"
     ): Call<GetMoviesResponse>
@@ -17,7 +17,7 @@ interface Api {
     //최고평점
     @GET("movie/top_rated")
     fun getTopRatedMovies(
-        @Query("api_key") apiKey: String = "b0b56c166ec5839d540382c3efb9113e",
+        @Query("api_key") apiKey: String = "키발급",
         @Query("page") page: Int,
         @Query("language") language : String = "ko"
     ): Call<GetMoviesResponse>
@@ -25,7 +25,7 @@ interface Api {
     //개봉 예정중인
     @GET("movie/upcoming")
     fun getUpcomingMovies(
-        @Query("api_key") apiKey: String = "b0b56c166ec5839d540382c3efb9113e",
+        @Query("api_key") apiKey: String = "키발급",
         @Query("page") page: Int,
         @Query("language") language : String = "ko"
     ): Call<GetMoviesResponse>
